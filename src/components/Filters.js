e70108cd61b983d69f681fde2269b8e145bb12ec
achtenberg167 <achtenberg16@gmail.com.br>
2022-03-22 17:23:51 -0300

    feat: implementa logica de filtrar as options

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,10 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import planetsContext from '../context/planetsContext';
 
+const OPTIONS_COLLUMN = ['population', 'orbital_period',
+  'diameter', 'rotation_period', 'surface_water'];
+
 export default function Filters() {
-  const { handleFilter, filters,
-  } = useContext(planetsContext);
-  const { filterByName: { name } } = filters;
+  const [filterNumeric, setInfos] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: '0',
+  });
+
+  const { handleFilter, filters } = useContext(planetsContext);
+
+  const { filterByName: { name }, filterByNumericValues } = filters;
+  const optionsColumnFilters = OPTIONS_COLLUMN
+    .filter((option) => !filterByNumericValues.some(({ column }) => column === option));
   return (
     <div>
       <label htmlFor="nameInput">
@@ -17,6 +28,15 @@ export default function Filters() {
           value={ name }
         />
       </label>
+      <form>
+        <select>
+          {optionsColumnFilters.map((optionName) => (
+            <option value={ optionName } key={ optionName }>
+              {optionName}
+            </option>
+          ))}
+        </select>
+      </form>
     </div>
   );
 }
